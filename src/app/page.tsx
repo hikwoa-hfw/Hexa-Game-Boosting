881065e42f8cb79de2826fd28e09d81d10af318a
hikwoa-hfw <hibbanhfw@gmail.com>
2024-11-17 18:51:49 +0700

    titik 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import MarqueeComponent from "@/components/MarqueeComponent";
-import ValueProposition from "@/components/ValueProposition";
-import CompanyOverview from "@/components/CompanyOverview";
-import CompanyTagline from "@/components/CompanyTagline";
-import Service from "@/components/Service";
-import Testimonials from "@/components/Testimonials";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import MarqueeComponent from "../components/MarqueeComponent";
+import ValueProposition from "../components/ValueProposition";
+import CompanyOverview from "../components/CompanyOverview";
+import CompanyTagline from "../components/CompanyTagline";
+import Service from "../components/Service";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
